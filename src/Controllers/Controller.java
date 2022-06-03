77c05dbc33c95901919d9fffdbe26c3169a25448
yoophglup <yoophglup@gmail.com>
2022-06-03 02:26:55 -0700

    Update

--- src/Controllers/Controller.java
+++ src/Controllers/Controller.java
@@ -1,4 +1,21 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
 public class Controller {
+    public Label outputbox;
+    public Button login;
+
+    public void quit_clicked(ActionEvent actionEvent) {
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+    public void validate(ActionEvent actionEvent) {
+        outputbox.setText("Username or \npasssword is \nincorrect.\nTry Again");
+    }
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -7,19 +7,23 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../Scenes/loginscreen.fxml"));
-        primaryStage.setTitle("Login if you can");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("SchedulingDesktopApplication");
+        primaryStage.setScene(new Scene(root, 320, 275));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
         JDBC.makeConnection();
+
         launch(args);
     }
 }
