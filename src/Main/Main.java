fd5321661074e1e0d67f687f1d8b51d2e68532c9
yoophglup <yoophglup@gmail.com>
2022-06-03 00:13:38 -0700

    Initial Commit

---
+++ src/Main/Main.java
@@ -0,0 +1,25 @@
+package Main;
+
+import Controllers.JDBC;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../Scenes/loginscreen.fxml"));
+        primaryStage.setTitle("Login if you can");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        JDBC.makeConnection();
+        launch(args);
+    }
+}
