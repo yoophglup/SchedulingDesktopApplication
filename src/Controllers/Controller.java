60c6f8985fa51ea6430460b5e5628953ecd55885
yoophglup <yoophglup@gmail.com>
2022-06-03 23:41:38 -0700

    Update

--- src/Controllers/Controller.java
+++ src/Controllers/Controller.java
@@ -6,9 +6,24 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.sql.PreparedStatement;
+import java.time.ZoneId;
+import java.util.Locale;
+
 public class Controller {
     public Label outputbox;
     public Button login;
+    public Label locationdata;
+
+    public void initialize(){
+        //Locale.setDefault(new Locale("fr"));
+
+        locationdata.setText(Locale.getDefault().toString());
+        String sql ="select * from users";
+
+    }
 
     public void quit_clicked(ActionEvent actionEvent) {
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
@@ -16,6 +31,7 @@ public class Controller {
     }
 
     public void validate(ActionEvent actionEvent) {
+
         outputbox.setText("Username or \npasssword is \nincorrect.\nTry Again");
     }
 }
